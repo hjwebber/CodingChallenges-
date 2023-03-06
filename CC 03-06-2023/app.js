2e3d57e4b7c47969549a7efefc31fa2f9336fc6f
unknown <hjwebber97@gmail.com>
2023-03-06 13:31:47 -0800

    Coding Challenge 3-06-2023 String Methods JS

---
+++ CC 03-06-2023/app.js
@@ -0,0 +1,11 @@
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+    let text=s
+    let result= text.repeat(n)
+    return result;
+  }
+
+  //clearly define params
+  //use repeat() string method
+  //return result
